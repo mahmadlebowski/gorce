611499cb986c316de725a42e3385f48aa38d9db6
mahmadlebowski <lejazzman1@gmail.com>
2021-01-19 15:40:06 +0000

    Updates

---
+++ precache-manifest.201543096bd29e7f3c126b830bbf5a9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c288f1350fc7a623a4076626b5f59d18",
+    "url": "/gorce/index.html"
+  },
+  {
+    "revision": "a699118576e81c9c9670",
+    "url": "/gorce/static/css/main.b5766442.chunk.css"
+  },
+  {
+    "revision": "082732d31556f8550ab5",
+    "url": "/gorce/static/js/2.0de065b6.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/gorce/static/js/2.0de065b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a699118576e81c9c9670",
+    "url": "/gorce/static/js/main.521bf461.chunk.js"
+  },
+  {
+    "revision": "52f01001a3e89436c445",
+    "url": "/gorce/static/js/runtime-main.d2694327.js"
+  }
+]);
